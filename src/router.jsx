fa3fa717bb6afcb9efac415326b62c377d1f1a98
shoab-asl <shoab@asl.aero>
2023-09-21 16:48:13 +0600

    updated

--- src/router.jsx
+++ src/router.jsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Dashboard from "./views/Dashboard";
 import Login from "./views/Login";
-
 import GuestLayout from "./components/GuestLayout";
 import PrivateLayout from "./components/PrivateLayout";
 import Contact from "./views/Contact";
