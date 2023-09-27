6a20ffa19c6cb15a97693676b403979e6baba463
shoab-asl <shoab@asl.aero>
2023-09-27 17:58:36 +0600

    updated

--- src/redux/loginApi.jsx
+++ src/redux/loginApi.jsx
@@ -1,11 +1,13 @@
 // api/authApi.js
 
 import axios from 'axios';
+import config from '../../config.js';
 
-export const loginApi = async (credentials) => {
+export const loginApi = async  (credentials) => {
+
   try {
-     const response=  axios.post(`http://192.168.30.17:8000/api/auth/`,credentials).then((response)=>{
-        console.log(response.data)
+     const response= await axios.post(`${config.API_URL}/api/auth/`,credentials).then((response)=>{
+        console.log(response.data);
     })
     
     return response.data;
