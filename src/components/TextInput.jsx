f6d274cd48c3167a91accb35b7ce3c2a13ca614a
Afroza <afroza@aslgroup.com.bd>
2023-09-27 14:50:51 +0600

    add functionality for forgotPassword and passwordReset page

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const TextInput = ({ id, name, type, placeholder, value, onChange, error }) => {
+const TextInput = ({ id, name, type, placeholder, value, onChange, error,disabled,readOnly }) => {
     return (
         <div >
             <input
@@ -9,6 +9,8 @@ const TextInput = ({ id, name, type, placeholder, value, onChange, error }) => {
                 name={name}
                 placeholder={placeholder}
                 value={value}
+                disabled={disabled}
+                readOnly={readOnly}
                 onChange={onChange}
                 className={`block w-full border-b-2 border-gray-300 py-1.5 text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm sm:leading-6 ${error ? 'border-red-500' : ''
                     }`}
--- src/views/PasswordReset.jsx
+++ src/views/PasswordReset.jsx
@@ -1,11 +1,75 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Image from "../assets/banelogo.png";
 import TextInput from "../components/TextInput";
 import { useLocation } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 
 const PasswordReset = () => {
   const location = useLocation();
-  const email = location.state;
+  const data = location.state;
+  const [forgotPasswordData, setForgotPasswordData] = useState({
+    email: data.email,
+    new_password: "",
+    confirm_password: "",
+  });
+
+  const [newPasswordVisible, setNewPasswordVisible] = useState(false);
+  const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
+  const [errors, setErrors] = useState({
+    new_password: "",
+    confirm_password: "",
+  });
+
+  //Set Form Data
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForgotPasswordData({
+      ...forgotPasswordData,
+      [name]: value,
+    });
+
+    // Clear the error message for the input field that changed
+    setErrors({
+      ...errors,
+      [name]: "",
+    });
+  };
+
+  // Form Submit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const passwordErrors = { new_password: "", confirm_password: "" };
+
+    if (!forgotPasswordData.new_password) {
+      passwordErrors.new_password = "New Password is required";
+    }
+
+    if (!forgotPasswordData.confirm_password) {
+      passwordErrors.confirm_password = "Confirm Password is required";
+    } else if (
+      forgotPasswordData.new_password !== forgotPasswordData.confirm_password
+    ) {
+      passwordErrors.confirm_password = "Passwords do not match";
+    }
+
+    setErrors(passwordErrors);
+
+    if (!passwordErrors.new_password && !passwordErrors.confirm_password) {
+      const data = {
+        email: forgotPasswordData.email,
+        password: forgotPasswordData.new_password,
+      };
+      console.log("Form submitted successfully", data);
+      // Reset the password fields
+      setForgotPasswordData({
+        ...forgotPasswordData,
+        new_password: "",
+        confirm_password: "",
+      });
+    }
+  };
 
   return (
     <div className="justify-center min-h-screen flex  items-center">
@@ -13,7 +77,7 @@ const PasswordReset = () => {
         <div className="flex justify-center">
           <img src={Image} alt="bane-logo" className="w-24 py-12" />
         </div>
-        <form className="space-y-4 ">
+        <form className="space-y-4" onSubmit={handleSubmit}>
           <div className="px-4">
             <label
               htmlFor="email"
@@ -26,8 +90,8 @@ const PasswordReset = () => {
                 id="email"
                 type="email"
                 name="email"
-                value={email.email}
-                disabled
+                value={forgotPasswordData.email}
+                readOnly={true}
               />
             </div>
           </div>
@@ -38,13 +102,21 @@ const PasswordReset = () => {
             >
               New Password
             </label>
-            <div className="mt-2">
+            <div className="mt-2 relative">
               <TextInput
                 id="new_password"
                 name="new_password"
-                type="password"
+                type={newPasswordVisible ? "text" : "password"}
+                value={forgotPasswordData.new_password}
                 autoComplete="password"
                 placeholder="Enter a New Password"
+                error={errors.new_password}
+                onChange={handleChange}
+              />
+              <FontAwesomeIcon
+                icon={newPasswordVisible ? faEyeSlash : faEye}
+                className="password-icon absolute right-0 top-2 text-gray-400 cursor-pointer"
+                onClick={() => setNewPasswordVisible(!newPasswordVisible)}
               />
             </div>
           </div>
@@ -55,13 +127,23 @@ const PasswordReset = () => {
             >
               Confirm Password
             </label>
-            <div className="mt-2">
+            <div className="mt-2 relative">
               <TextInput
                 id="confirm_password"
                 name="confirm_password"
-                type="password"
+                type={confirmPasswordVisible ? "text" : "password"}
+                value={forgotPasswordData.confirm_password}
                 autoComplete="password"
                 placeholder="Confirm Password"
+                error={errors.confirm_password}
+                onChange={handleChange}
+              />
+              <FontAwesomeIcon
+                icon={confirmPasswordVisible ? faEyeSlash : faEye}
+                className="password-icon absolute right-0 top-2 text-gray-400 cursor-pointer"
+                onClick={() =>
+                  setConfirmPasswordVisible(!confirmPasswordVisible)
+                }
               />
             </div>
           </div>
@@ -81,4 +163,3 @@ const PasswordReset = () => {
 };
 
 export default PasswordReset;
-sssssssssssssssssssssssssssssssssssssssssss
